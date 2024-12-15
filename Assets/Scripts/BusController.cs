bd00296c3cbe00f999d6a783ee3a10741912674f
MameMorM <mamemorm@my.yorku.ca>
2024-12-15 01:51:15 -0500

    Update 5.1
    - Uploaded missing files

--- Assets/Scripts/BusController.cs
+++ Assets/Scripts/BusController.cs
@@ -1,7 +1,6 @@
 
 using System;
 using System.Collections.Generic;
-using UnityEditor.U2D;
 using UnityEngine;
 
 public class BusController : MonoBehaviour
--- Assets/Scripts/PassengerText.cs
+++ Assets/Scripts/PassengerText.cs
@@ -9,7 +9,9 @@ public class PassengerText : MonoBehaviour
 
     public BusCapacity busCapacity;
     public BusController bus;
+    private int topSpeed;
     public Text passText;
+    public Text gradeText;
     public Text speedText;
     // Start is called before the first frame update
     void Start()
@@ -22,14 +24,66 @@ public class PassengerText : MonoBehaviour
     void Update()
     {
         passText.text = ("Passengers: " + busCapacity.passengers/2).ToString();
-        
-        if(bus.speed < 0)
+        if (busCapacity.passengers / 2 >= 15)
         {
-            speedText.text = (bus.speed*-1).ToString() + " KM/H";
+            gradeText.text = "A+";
+        }
+
+        else if (busCapacity.passengers / 2 < 15 && busCapacity.passengers / 2 > 12)
+        {
+            gradeText.text = "A";
+        }
+
+        else if (busCapacity.passengers / 2 < 15 && busCapacity.passengers / 2 >= 12)
+        {
+            gradeText.text = "B+";
+        }
+
+        else if (busCapacity.passengers / 2 < 12 && busCapacity.passengers / 2 >= 10)
+        {
+            gradeText.text = "B";
+        }
+
+        else if (busCapacity.passengers / 2 < 10 && busCapacity.passengers / 2 >= 8)
+        {
+            gradeText.text = "C+";
+        }
+
+        else if (busCapacity.passengers / 2 < 8 && busCapacity.passengers / 2 >= 6)
+        {
+            gradeText.text = "C";
+        }
+        else if (busCapacity.passengers / 2 < 6 && busCapacity.passengers / 2 >= 4)
+        {
+            gradeText.text = "D+";
+        }
+
+        else if (busCapacity.passengers / 2 < 4 && busCapacity.passengers / 2 >= 2)
+        {
+            gradeText.text = "D";
+        }
+
+        else
+        {
+            gradeText.text = "F";
+        }
+
+        if (topSpeed < bus.speed/50)
+        {
+            topSpeed = bus.speed/50 ;
+        }
+
+        if (bus.speed < 0 && SceneManager.GetActiveScene().name != "EndGrade")
+        {
+            speedText.text = ((bus.speed*-1)/50).ToString() + " KM/H";
+        }
+        else if (bus.speed > 0 && SceneManager.GetActiveScene().name != "EndGrade")
+        {
+            speedText.text = (bus.speed/50).ToString() + " KM/H";
         }
         else
         {
-            speedText.text = bus.speed.ToString() + " KM/H";
+            speedText.text = topSpeed.ToString() + "KM/H";
         }
 
         if(Input.GetKeyDown(KeyCode.Escape))
